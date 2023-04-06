ac64bf7e02c4bad71912a970c301ba8f65232977
Laybring <dimarulyov@gmail.com>
2023-04-06 14:20:00 +0300

    Добавьте файлы проекта.

---
+++ Pomogite_3/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Pomogite_3
+{
+    public partial class Form1 : Form
+    {
+
+        DBconnect connection = new DBconnect();
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            connection.ConOpen();
+
+            SqlCommand command_data = new SqlCommand("select * from USER_", connection.GetConnection());
+
+            dataGridView1.Columns.Add("S1","ФИО");
+            dataGridView1.Columns.Add("S2", "Номер");
+            dataGridView1.Columns.Add("S3", "Адресс");
+            dataGridView1.Columns.Add("S4", "Паспортные данные");
+
+            using (var reader = command_data.ExecuteReader())
+            {
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(reader["FIO"].ToString(), reader["NUMBER"].ToString(), reader["ADRESS"].ToString(), reader["PASS_DAN"].ToString());
+                }
+
+            }
+
+            FormBorderStyle = FormBorderStyle.None;
+
+            connection.ConClosed();
+        }
+
+        //Добавить
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            panel4.Enabled= true;
+
+        }
+
+        //Реактировать
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            UpdateUser updateUser = new UpdateUser();
+            updateUser.Show();
+            this.Hide();
+
+        }
+
+        //Удалить
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            connection.ConOpen();
+
+            string name = dataGridView1.CurrentCell.Value.ToString();
+
+            SqlCommand command_4 = new SqlCommand($"DELETE FROM USER_ WHERE FIO = '{name}' or NUMBER ='{name}' or ADRESS = '{name}' or PASS_DAN = '{name}';", connection.GetConnection());
+            command_4.ExecuteNonQuery();
+            MessageBox.Show("Запись успешно удална");
+
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            connection.ConOpen();
+
+            SqlCommand command_data = new SqlCommand("select * from USER_", connection.GetConnection());
+
+            dataGridView1.Columns.Add("S1", "ФИО");
+            dataGridView1.Columns.Add("S2", "Номер");
+            dataGridView1.Columns.Add("S3", "Адресс");
+            dataGridView1.Columns.Add("S4", "Паспортные данные");
+
+            using (var reader = command_data.ExecuteReader())
+            {
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(reader["FIO"].ToString(), reader["NUMBER"].ToString(), reader["ADRESS"].ToString(), reader["PASS_DAN"].ToString());
+                }
+
+            }
+            connection.ConClosed();
+
+        }
+        //Добавить в панели 4
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                connection.ConOpen();
+
+                SqlCommand command_1 = new SqlCommand($"INSERT INTO USER_ VALUES('{textBox2.Text}','{textBox3.Text}','{textBox4.Text}','{textBox5.Text}');", connection.GetConnection());
+                command_1.ExecuteNonQuery();
+                MessageBox.Show("Запись добавлена успешно");
+
+                panel4.Enabled = false;
+
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+
+                connection.ConClosed();
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Программная ошибка");
+            }
+        }
+
+        //Выйти в панели 4
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            panel4.Enabled = false;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            connection.ConOpen();
+
+            dataGridView1.Rows.Clear();
+
+            SqlCommand command = new SqlCommand($"SELECT * FROM USER_ WHERE FIO LIKE '%{textBox1.Text}%';", connection.GetConnection());
+            using (var reader = command.ExecuteReader())
+            {
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(reader["FIO"].ToString(), reader["NUMBER"].ToString(), reader["ADRESS"].ToString(), reader["PASS_DAN"].ToString());
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Pomogite_3/UpdateUser.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Contracts;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pomogite_3
+{
+    public partial class UpdateUser : Form
+    {
+
+        DBconnect connection = new DBconnect();
+
+        public UpdateUser()
+        {
+            InitializeComponent();
+
+            FormBorderStyle = FormBorderStyle.None;
+
+            connection.ConOpen();
+
+            SqlCommand command_1 = new SqlCommand("select * from USER_;", connection.GetConnection());
+
+            using (var reader = command_1.ExecuteReader())
+            {
+
+                while (reader.Read())
+                {
+
+                    comboBox1.Items.Add(reader["FIO"].ToString());
+
+                }
+
+            }
+
+            SqlCommand command_data = new SqlCommand("select * from USER_", connection.GetConnection());
+
+            dataGridView1.Columns.Add("S1", "ФИО");
+            dataGridView1.Columns.Add("S2", "Номер");
+            dataGridView1.Columns.Add("S3", "Адресс");
+            dataGridView1.Columns.Add("S4", "Паспортные данные");
+
+            using (var reader = command_data.ExecuteReader())
+            {
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(reader["FIO"].ToString(), reader["NUMBER"].ToString(), reader["ADRESS"].ToString(), reader["PASS_DAN"].ToString());
+                }
+
+            }
+
+            textBox1.Enabled = false;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand command_2 = new SqlCommand($"UPDATE USER_ SET {comboBox2.Text} = '{textBox1.Text}' WHERE FIO = '{comboBox1.Text}';", connection.GetConnection());
+                command_2.ExecuteNonQuery();
+                MessageBox.Show("Вы успешно изсмнили данные");
+                Form1 form1 = new Form1();
+                form1.Show();
+                this.Hide();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("У вас не выбрано поле с именем или рдактируемой колонкой");
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+        }
+    }
+}
